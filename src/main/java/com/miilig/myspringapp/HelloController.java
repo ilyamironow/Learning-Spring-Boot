d0834ea4a0a181e60b61d1c5cb571eb7e65e16e1
Ilia Mironov <imironov@devexperts.com>
2023-12-16 11:17:28 +0400

    sample app that has my bean and my rest endpoint and unit+integration tests

---
+++ src/main/java/com/miilig/myspringapp/HelloController.java
@@ -0,0 +1,6 @@
+package com.miilig.myspringapp;/**
+ * @author imironov
+ * @since 14.12.2023
+ */
+public class HelloController {
+}
---
+++ src/test/java/com/miilig/myspringapp/MySpringApplicationContextTest.java
@@ -0,0 +1,13 @@
+package com.miilig.myspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
